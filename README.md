a5c1da00856585a9e9da2a0af536bb72b9ec44ff
Gavin <shellsuperhu@gmail.com>
2022-04-26 15:19:20 +0800

    add scripts all

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This script will export windterm session from zoc ini file.
 
-#### Usage
+#### zoc Usage
 1. export zoc ini file 'HostDirectory.zocini'
 2. run script get windterm session
 ```shell script
@@ -8,6 +8,25 @@ python zoc_to_windterm.py |python -m json.tool
 ```
 3. add session text to windterm session file (eg: ~/.wind/profiles/default.v10/terminal/user.sessions)
 
+
+
+#### iterm2 Usage
+1. export iterm2 json file 'Profiles.json'
+2. run script get windterm session
+```shell script
+python iterm2_to_windterm.py |python -m json.tool
+```
+3. add session text to windterm session file (eg: ~/.wind/profiles/default.v10/terminal/user.sessions)
+
+
+
+#### securecrt Usage
+1. export securecrt xml file 'crt.xml'
+2. run script get windterm session
+```shell script
+python securecrt_to_windterm.py |python -m json.tool
+```
+3. add session text to windterm session file (eg: ~/.wind/profiles/default.v10/terminal/user.sessions)
 
 
 
--- securecrt_to_windterm.py
+++ securecrt_to_windterm.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-import re
 import json
 import uuid
-import xml.etree.ElementTree as ET
 import xml.dom.minidom as minidom
+
 
 
 def get_sessions_child_nodes_elements():
@@ -17,9 +16,9 @@ def get_sessions_child_nodes_elements():
         if e.getAttribute("name") == "Sessions":
             sessions_element = e
             break
-    for e in sessions_element.childNodes:
-        if e.nodeType != e.TEXT_NODE:
-            child_nodes_elements.append(e)
+    for node in sessions_element.childNodes:
+        if node.nodeType == node.ELEMENT_NODE:
+            child_nodes_elements.append(node)
     return child_nodes_elements
 
 
@@ -43,8 +42,10 @@ def recursion_get_child_nodes_by_element(element, hosts_dict, tree_dict):
 
         for e in element.getElementsByTagName("string"):
             if e.getAttribute("name") == "Hostname":
-                session_target = e.firstChild.nodeValue
-                break
+                session_target = ""
+                if e.firstChild is not None:
+                    session_target = e.firstChild.nodeValue
+                    break
 
         host_dict = {
             "session.autoLogin": "",
@@ -60,6 +61,7 @@ def recursion_get_child_nodes_by_element(element, hosts_dict, tree_dict):
         for child_element in child_elemets:
             parent_label = cut_label
             child_label = child_element.getAttribute("name")
+            # parent_label = unicode(cut_label, encoding="utf-8")
             t_dict = {
                 "parent_name": parent_label,
                 "name": child_label
@@ -86,12 +88,12 @@ def print_windterm_json():
     hosts = []
     for child_node_element in child_nodes_elements:
         child_label = child_node_element.getAttribute("name")
-        child_nodes_dict = {}
         hosts_dict = {}
-        tree_dict = {}
-        tree_dict[child_label] = {
-            "parent_name": "0",
-            "name": child_label
+        tree_dict = {
+            child_label: {
+                "parent_name": "0",
+                "name": child_label
+            }
         }
         recursion_get_child_nodes_by_element(child_node_element, hosts_dict, tree_dict)
 
@@ -99,14 +101,14 @@ def print_windterm_json():
             full_paths_folder = []
             recursion_get_full_paths_folder(session_label, tree_dict, full_paths_folder)
 
-            session_group = ">".join(map(str, full_paths_folder))
+            session_group = ">".join(map(str, full_paths_folder[:-1]))
             session["session.group"] = session_group
             hosts.append(session)
 
         # print(json.dumps(hosts_dict))
         # print(json.dumps(tree_dict))
-        break
-    print(json.dumps(hosts))
+        # break
+    print(json.dumps(hosts, ensure_ascii=False))
 
 
 if __name__ == "__main__":
